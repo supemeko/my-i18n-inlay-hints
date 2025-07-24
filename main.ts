de03918e60a49ff0ff0676cbb664d873b0956767
gary go <supemeko@gmail.com>
2025-07-25 03:54:28 +0800

    update

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 // deno-lint-ignore-file no-explicit-any
+import * as path from "https://deno.land/std/path/mod.ts";
 import { log, setLogFile } from "./log.ts";
 import { serve } from "./serve.ts";
 import { actions as inlayHint } from "./inlayHint.ts";
 import { actions as documents } from "./documents.ts";
-
 const initialize = {
   initialize: (params: any, id: any) => {
     log(`Initialize request: ${JSON.stringify(params)}`);
@@ -22,7 +22,13 @@ const initialize = {
 
 const actions: any = { ...inlayHint, ...documents, ...initialize };
 
-setLogFile("./io-log.log", "./log.log");
+// 获取可执行文件路径
+const execPath = Deno.execPath();
+const extPath = path.dirname(execPath);
+// 获取可执行文件所在目录
+// const execDir = .dirname();
+setLogFile(`${extPath}/io-log.log`, `${extPath}/log.log`);
+log("Executable directory mm:" + path.dirname(execPath));
 
 serve(async (message: string) => {
   try {
